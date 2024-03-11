b417563bdda4638d4d9c50ac24819a662bd8d5bc
Victoria Pruitt <victoriapruittart@gmail.com>
2024-03-11 15:04:23 -0500

    figuring it out surely

--- Assets/Scripts/Customers/Counter.cs
+++ Assets/Scripts/Customers/Counter.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static Station;
 
 public class Counter : MonoBehaviour
 {
+    public bool _counterIsEmpty = true;
+    PACPointer PACPointer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        PACPointer = GameObject.FindObjectOfType(typeof(PACPointer)) as PACPointer;
     }
 
     // Update is called once per frame
--- Assets/Scripts/Customers/Customer.cs
+++ Assets/Scripts/Customers/Customer.cs
@@ -156,7 +156,7 @@ public class Customer : MonoBehaviour
         if (wasOrderCompleted == true)
         {
             //happy pop up, walk away, destroy
-            
+             
         }
         else
         {
