f6c77f25e65eb8d924e279cb3bdaedce188b0043
Kellyn Mendez <kpm.mendez@gmail.com>
2024-03-16 00:13:27 -0500

    3D ASSEMBLY WORKS WHOOP WHOOP

--- Assets/Scripts/Utility/Draggable3DItem.cs
+++ Assets/Scripts/Utility/Draggable3DItem.cs
@@ -9,12 +9,11 @@ public class Draggable3DItem : MonoBehaviour
     private Vector3 startPosition;
     private Vector3 mousePosition;
     private bool dropped = false;
-    private float planeY = 0;
     [SerializeField] Camera cmra;
 
-    private void Awake()
+    public void Awake()
     {
-        planeY = transform.position.y;
+        startPosition = transform.position;
     }
 
     private Vector3 GetMousePos()
@@ -33,29 +32,25 @@ public class Draggable3DItem : MonoBehaviour
         if (dropped) 
             return;
 
-        /*Transform draggingObject = transform;
-
-        Plane plane = new Plane(Vector3.up, Vector3.up * planeY); // ground plane
-
-        Ray ray = cmra.ScreenPointToRay(Input.mousePosition);
-
-        float distance; // the distance from the ray origin to the ray intersection of the plane
-        if (plane.Raycast(ray, out distance))
-        {
-            draggingObject.position = ray.GetPoint(distance); // distance along the ray
-        }*/
-
         transform.position = cmra.ScreenToWorldPoint(Input.mousePosition - mousePosition);
+    }
+
+    public void OnMouseUp()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(cmra.ScreenPointToRay(Input.mousePosition), out hit))
+        {
+            DropPoint3D slot = hit.collider.GetComponent<DropPoint3D>();
+            if (slot != null && slot.slotName == transform.name)
+            {
+                slot.SnapToSlot(this);
+            }
+        }
     }
 
     public Vector3 GetStartPosition()
     {
         return startPosition;
-    }
-
-    public void SetStartPosition(Vector3 startPosition)
-    { 
-        this.startPosition = startPosition;
     }
 
     public void SetDropped(bool dropped)
---
+++ Assets/Scripts/Utility/DropPoint3D.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropPoint3D : MonoBehaviour
+{
+    [SerializeField] public string slotName;
+
+    private bool dropped = false;
+    private Draggable3DItem draggable;
+    private Vector3 origStartPos;
+    private Collider dropCollider;
+
+    private void Awake()
+    {
+        dropCollider = GetComponent<Collider>();
+    }
+
+    public void SnapToSlot(Draggable3DItem draggable)
+    {
+        this.draggable = draggable;
+        origStartPos = draggable.GetStartPosition();
+        draggable.transform.position = transform.position;
+        dropCollider.enabled = false;
+        draggable.SetDropped(true);
+        dropped = true;
+    }
+
+    public void SetSlotName(string slotName)
+    {
+        this.slotName = slotName;
+    }
+
+    public bool Filled()
+    {
+        return dropped;
+    }
+
+    public void ResetDropPoint()
+    {
+        draggable.transform.position = origStartPos;
+        dropCollider.enabled = true;
+        draggable.SetDropped(false);
+        draggable = null;
+        dropped = false;
+    }
+}
